b458efd215d7bd89b84a55ed4dde1c107fb9f7a6
brachy84 <brachy84@gmail.com>
2021-06-27 06:07:49 +0200

    add wthit to dev env

---
+++ src/main/java/com/github/brachy84/wthitplusplus/WthitPlugin.java
@@ -0,0 +1,11 @@
+package com.github.brachy84.wthitplusplus;
+
+import mcp.mobius.waila.api.IRegistrar;
+import mcp.mobius.waila.api.IWailaPlugin;
+
+public class WthitPlugin implements IWailaPlugin {
+    @Override
+    public void register(IRegistrar registrar) {
+
+    }
+}
