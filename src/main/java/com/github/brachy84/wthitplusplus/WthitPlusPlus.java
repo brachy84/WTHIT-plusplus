68155054e6c221cde748712a15ce73a8d90f2be9
brachy84 <brachy84@gmail.com>
2021-06-27 06:03:04 +0200

    setup dev env

---
+++ src/main/java/com/github/brachy84/wthitplusplus/WthitPlusPlus.java
@@ -0,0 +1,11 @@
+package com.github.brachy84.wthitplusplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WthitPlusPlus implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
